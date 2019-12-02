3a8e9b712194d3cc80928acc5e5376afa395bc59
Velizar Hristov <velizar.hristov@67bricks.com>
2019-12-02 13:55:37 +0000

    Add solution to problem 1, part 1

--- src/1.rs
+++ src/1.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn fuel_for_module_mass(module_mass: i64) -> i64 {
+    module_mass / 3 - 2
+}
+
 fn main() {
-    println!("Hello, worlde2!");
+    let file = File::open("1.txt").expect("Unable to open file");
+    let br = BufReader::new(file);
+    let data: Vec<i64> = br.lines()
+        .map(|line| {
+            let str = line.expect("Could not parse line");
+            let num = str.parse::<i64>().unwrap();
+            fuel_for_module_mass(num)
+        })
+        .collect();
+
+    let total: i64 = data.iter().sum();
+    println!("{}", total);
 }
