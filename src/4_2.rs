9f76e8ed741bc344779f7a73a74dc42a33f9cbb7
Velizar Hristov <velizar.hristov@67bricks.com>
2019-12-05 11:49:50 +0000

    Add a comment comparing Rust to Scala

--- src/4_2.rs
+++ src/4_2.rs
@@ -1,3 +1,12 @@
+/** Hideous! This is the same code in Scala.
+ *
+ * def hasGroupOfTwo(str: String): Boolean = {
+ *   val sz = str.takeWhile(_ == str.head).size
+ *   sz == 2 || (!str.drop(sz).isEmpty && hasGroupOfTwo(str.drop(sz)))
+ * }
+ *
+ * Probably Advent of Code is not the right problem for Rust.
+ */
 fn has_group_of_two(string: &String) -> bool {
     let first_char = string.chars().nth(0).unwrap();
     let subsequence_size = string.chars().take_while(|c|
