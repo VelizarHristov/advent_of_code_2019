b2355ffb3e6fdbe774d9a445c269df599221dfba
Velizar Hristov <velizar.hristov@67bricks.com>
2019-12-06 12:43:14 +0000

    Simplify 4_2 and remove comment about Rust being verbose

--- src/4_2.rs
+++ src/4_2.rs
@@ -1,20 +1,8 @@
-/** Hideous! This is the same code in Scala.
- *
- * def hasGroupOfTwo(str: String): Boolean = {
- *   val sz = str.takeWhile(_ == str.head).size
- *   sz == 2 || (!str.drop(sz).isEmpty && hasGroupOfTwo(str.drop(sz)))
- * }
- *
- * Probably Advent of Code is not the right problem for Rust.
- */
 fn has_group_of_two(string: &String) -> bool {
-    let first_char = string.chars().nth(0).unwrap();
-    let subsequence_size = string.chars().take_while(|c|
-        *c == first_char
-    ).collect::<Vec<_>>().len();
-    let remaining = &string.chars().skip(subsequence_size).collect();
-    subsequence_size == 2 ||
-        (!remaining.is_empty() && has_group_of_two(remaining))
+    let first_char = string.chars().next().unwrap();
+    let sz = string.chars().take_while(|c| *c == first_char).count();
+    let remaining: &String = &string.chars().skip(sz).collect();
+    sz == 2 || (!remaining.is_empty() && has_group_of_two(remaining))
 }
 
 fn matches_criteria(i: &i32) -> bool {
