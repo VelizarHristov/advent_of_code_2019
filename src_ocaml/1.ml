7f75beb00461c818cee5c7eff079f8237be0f02d
Velizar Hristov <velizar.hs@gmail.com>
2019-12-08 12:16:09 +0000

    Problem 1.1 - refactor OCaml code to avoid print_int

--- src_ocaml/1.ml
+++ src_ocaml/1.ml
@@ -6,4 +6,5 @@ let fuel_per_module module_weight = module_weight / 3 - 2
 let () =
     let lines = In_channel.read_lines "../1.txt" in
     let module_weights = List.map ~f:(Fn.compose fuel_per_module int_of_string) lines in
-    print_int (List.reduce_exn ~f:(+) module_weights)
+    let total = (List.reduce_exn ~f:(+) module_weights) in
+    print_endline (string_of_int total)
