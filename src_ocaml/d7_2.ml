7cf8aac224e510324478a374dc2d567e88ba944f
Velizar Hristov <velizar.hs@gmail.com>
2019-12-14 14:35:48 +0000

    Solve Day 7 part 2

--- src_ocaml/d7_2.ml
+++ src_ocaml/d7_2.ml
@@ -2,69 +2,71 @@ open Batteries
 open Core
 open Printf
 
-let print_state state = BatVect.fold (fun a b -> a ^ "," ^ (string_of_int b)) "" state |> print_endline
+let print_code code =
+  BatVect.map string_of_int code |>
+  BatVect.reduce (fun a b -> a ^ "," ^ b) |>
+  print_endline
 
-type machine_state = { numbers: int BatVect; inputs: int list; idx: int }
+type machine_state = { code: int BatVect.t; idx: int; inputs: int list; output: int option }
 
-(*
-  output: (int, int -> int) - the first is the opcode,
-  the second returns the parameter mode of the ith parameter, or 0 if i is out of boundaries
-*)
 let parse_opcode raw_opcode =
   let chars = String.to_list (string_of_int raw_opcode) in
   let opcode =
     let opcode_chars = List.drop chars ((List.length chars) - 2) in
     int_of_string (String.of_char_list opcode_chars) in
   let param_mode_chars = List.take chars ((List.length chars) - 2) |> List.rev in
-  let get_param = fun i ->
+  let get_param_mode = fun i ->
     let retrieved = List.nth param_mode_chars i in
     Option.value retrieved ~default:'0' in
-  (opcode, get_param)
+  (opcode, get_param_mode)
 
-let apply_op opcode param_modes program_inputs state idx =
-  let get = BatVect.get state in
-  let get_input i = match param_modes i with
-    | '0' -> get (get (idx + i + 1))
-    | '1' -> get (idx + i + 1)
-    | _ -> raise (Failure "Invalid param code") in
+let get_param_from code param_modes idx i = match param_modes (i - 1) with
+    | '0' -> BatVect.get code (BatVect.get code (idx + i))
+    | '1' -> BatVect.get code (idx + i)
+    | _ -> raise (Failure "Invalid param code")
+
+let apply_op opcode get_param state =
+  let {code; idx; inputs; _} = state in
+  let get_param_idx i = BatVect.get code (idx + i) in
   match opcode with
   | 1 ->
-    let output_idx = get (idx + 3) in
-    let output = (get_input 0) + (get_input 1) in
-    ((BatVect.set state output_idx output), false, None, idx + 4)
+    let output = (get_param 1) + (get_param 2) in
+    let new_code = (BatVect.set code (get_param_idx 3) output) in
+    {state with idx = idx + 4; code = new_code}
   | 2 ->
-    let output_idx = get (idx + 3) in
-    let output = (get_input 0) * (get_input 1) in
-    ((BatVect.set state output_idx output), false, None, idx + 4)
+    let output = (get_param 1) * (get_param 2) in
+    let new_code = (BatVect.set code (get_param_idx 3) output) in
+    {state with idx = idx + 4; code = new_code}
   | 3 ->
-    let output_idx = get (idx + 1) in
-    ((BatVect.set state output_idx (List.hd_exn program_inputs)), true, None, idx + 2)
-  | 4 -> (state, false, Some (get_input 0), idx + 2)
-  | 5 ->
-    let next_idx = if (get_input 0) <> 0 then (get_input 1) else idx + 3 in
-    (state, false, None, next_idx)
-  | 6 ->
-    let next_idx = if (get_input 0) = 0 then (get_input 1) else idx + 3 in
-    (state, false, None, next_idx)
+    let next_input::remaining_inputs = inputs in
+    let new_code = (BatVect.set code (get_param_idx 1) next_input) in
+    {state with idx = idx + 2; code = new_code; inputs = remaining_inputs}
+  | 4 -> {state with idx = idx + 2; output = Some (get_param 1)}
+  | 5 -> {state with idx = if (get_param 1) <> 0 then (get_param 2) else idx + 3}
+  | 6 -> {state with idx = if (get_param 1) = 0 then (get_param 2) else idx + 3}
   | 7 ->
-    let output_idx = get (idx + 3) in
-    let output = if (get_input 0 < get_input 1) then 1 else 0 in
-    ((BatVect.set state output_idx output), false, None, idx + 4)
+    let output = if (get_param 1 < get_param 2) then 1 else 0 in
+    let new_code = (BatVect.set code (get_param_idx 3) output) in
+    {state with idx = idx + 4; code = new_code}
   | 8 ->
-    let output_idx = get (idx + 3) in
-    let output = if (get_input 0 = get_input 1) then 1 else 0 in
-    ((BatVect.set state output_idx output), false, None, idx + 4)
+    let output = if (get_param 1 = get_param 2) then 1 else 0 in
+    let new_code = (BatVect.set code (get_param_idx 3) output) in
+    {state with idx = idx + 4; code = new_code}
   | _ -> raise (Failure "Invalid opcode")
 
-let rec run_intcode_inner state program_inputs cur_idx =
-    let raw_opcode = BatVect.get state cur_idx in
-    let (opcode, param_modes) = parse_opcode raw_opcode in
-    if opcode = 99 then
-      None
+let rec run_intcode state =
+  let {code; idx; _} = state in
+  let raw_opcode = BatVect.get code idx in
+  let (opcode, param_modes) = parse_opcode raw_opcode in
+  if opcode = 99 then
+    state
+  else
+    let get_param = get_param_from code param_modes idx in
+    let next_state = apply_op opcode get_param state in
+    if (Option.is_some next_state.output) then
+      next_state
     else
-      let (next_state, input_was_read, program_output, next_idx) = apply_op opcode param_modes program_inputs state cur_idx in
-      let next_program_inputs = if input_was_read then List.tl_exn program_inputs else program_inputs in
-      (next_state, next_program_inputs, program_output, next_idx) in
+      run_intcode next_state
 
 let drop_nth ls n = List.take ls n @ List.drop ls (n + 1)
 let selections ls = List.map ~f:(fun i -> (List.nth_exn ls i, drop_nth ls i)) (List.range 0 (List.length ls))
@@ -73,35 +75,26 @@ let rec permutations ls = if List.is_empty ls then [[]] else
         List.map ~f:(fun perm -> first :: perm) (permutations rest)
     ) (selections ls) |> List.concat
 
-let rec repeat n e = if n = 0 then [] else e :: (repeat (n - 1) e)
-
-(* let rec run_cyclic states last_output phase_settings =
-  let iter = List.fold_until ~init:(states, last_output) ~f:(fun (cur_state::states, input_1) phase_setting ->
-    match run_intcode cur_state (phase_setting @ [input_1]) with
-      | (Some output, next_state) -> Continue (states @ [next_state], output)
-      | (None, _) -> Stop None
-    ) ~finish:(fun output_and_next_states -> Some output_and_next_states) phase_settings in
-  match iter with
-    | Some (next_states, next_output) -> run_cyclic next_states next_output (List.map ~f:(fun _ -> []) phase_settings)
-    | None -> last_output *)
-
-let rec run_cyclic states inputs =
-  let iter = List.fold_until ~init:(states, inputs) ~f:(fun (cur_state::states, input::next_inputs) _ ->
-    match run_intcode cur_state [input] with
-      | (Some output, next_state) -> Continue (states @ [next_state], next_inputs @ [output])
-      | (None, _) -> Stop None
-    ) ~finish:(fun output_and_next_states -> Some output_and_next_states) (List.range 0 5) in
-  match iter with
-    | Some (next_states, next_inputs) -> run_cyclic next_states next_inputs
-    | None -> List.last_exn inputs
+let rec run_cyclic states last_output =
+  let (next_states, output) = List.fold_left ~init:([], Some last_output) ~f:(fun (states, prev_output) state ->
+    let new_inputs = state.inputs @ (Option.to_list prev_output) in
+    let next_state = run_intcode { state with inputs = new_inputs; output = None } in
+    (states @ [next_state], next_state.output)
+  ) states in
+  match output with
+    | Some o -> run_cyclic next_states o
+    | None -> last_output
 
 let () =
-    let start_state =
+    let start_code =
       let file_input = In_channel.read_lines "../7.txt" |> List.hd_exn in
       List.map ~f:int_of_string (String.split file_input ~on:',') |> BatVect.of_list in
-    let start_states = repeat 5 start_state in
     let phase_setting_permutations = permutations (List.range 5 10) in
-    (* Assumes that the max permutation's output is more than 0? *)
-    let all_outputs = List.map ~f:(fun inputs -> run_cyclic start_states (inputs @ [0])) phase_setting_permutations in
-    let max_output = (List.reduce_exn ~f:(Stdlib.max) all_outputs) in
+    let all_outputs = List.map ~f:(fun inputs ->
+      let start_states = List.map ~f:(fun input ->
+        { code = start_code; idx = 0; inputs = [input]; output = None }
+      ) inputs in
+      run_cyclic start_states 0
+    ) phase_setting_permutations in
+    let max_output = (List.reduce_exn ~f:max all_outputs) in
     print_endline (string_of_int max_output)
